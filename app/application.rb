70f5f8f116fa261ca00eb7ce9df0cc895c61e68c
Derric Fragosa <fderric@gmail.com>
2020-04-24 21:49:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,33 +1,6 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
-  def call(env)
-    resp = Rack::Response.new
-    req = Rack::Request.new(env)
-
-    if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-      elsif req.path.match(/cart/)
-      if @@cart.empty?
-        resp.write "Your cart is empty"
-      else
-        @@cart.each do |item|
-          resp.write "#{item}\n"
-        end
-      end
-    elsif req.path.match(/add/)
-      item_to_add = req.params["item"]
-      if @@items.include? item_to_add
-        @@cart << item_to_add
-        resp.write "add #class Application
-
-  @@items = ["Apples","Carrots","Pears"]
   @@cart = []
 
   def call(env)
@@ -57,24 +30,6 @@ class Application
       else
         resp.write "We don't have that item!"
       end
-    else
-      resp.write "Path Not Found"
-    end
-
-    resp.finish
-  end
-
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
-end
-{item_to_add}"
-      else
-        resp.write "We don't have that item!"
     else
       resp.write "Path Not Found"
     end
